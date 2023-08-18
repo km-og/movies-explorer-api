00f27b432abec672ce14e0d443236f3f0965ca46
Inessa Kim <kim.inessa20@yandex.ru>
2023-08-18 13:58:41 +0300

    fix: исправлен порт

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const router = require("./routes");
 const errorHandler = require("./middlewares/errorHandler");
 const { requestLogger, errorLogger } = require("./middlewares/logger");
 
+const { PORT = 3000 } = process.env;
 const app = express();
 
 mongoose.connect("mongodb://localhost:27017/bitfilmsdb");
