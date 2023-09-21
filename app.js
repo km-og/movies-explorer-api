d4409d2bd87606778bffb2a4835a4b70160fac59
Inessa Kim <kim.inessa20@yandex.ru>
2023-09-21 13:09:39 +0300

    fix: добавлен cors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable no-console */
 // require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const helmet = require("helmet");
 const { errors } = require("celebrate");
+const cors = require("cors");
 const router = require("./routes");
 const errorHandler = require("./middlewares/errorHandler");
 const { requestLogger, errorLogger } = require("./middlewares/logger");
@@ -18,7 +20,7 @@ mongoose.connect(DB);
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(cors());
 app.get("/crash-test", () => {
   setTimeout(() => {
     throw new Error("Сервер сейчас упадёт");
