230ef00f2e77f96a39f484a7c70b51c9dbbb7dda
Inessa Kim <kim.inessa20@yandex.ru>
2023-10-14 22:46:00 +0300

    fix: cors

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+/* eslint-disable prefer-arrow-callback */
+/* eslint-disable func-names */
+/* eslint-disable indent */
+/* eslint-disable no-trailing-spaces */
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable no-console */
 // require("dotenv").config();
@@ -5,30 +9,33 @@ const express = require("express");
 const mongoose = require("mongoose");
 const helmet = require("helmet");
 const { errors } = require("celebrate");
-const cors = require("cors");
+// const cors = require("cors");
 const router = require("./routes");
 const errorHandler = require("./middlewares/errorHandler");
 const { requestLogger, errorLogger } = require("./middlewares/logger");
 const { DB, SERVER_PORT } = require("./utils/config");
+const cors = require("./middlewares/cors");
 
 // const { PORT = 3000 } = process.env;
 const app = express();
 
 mongoose.connect(DB);
 
-app.use(
-  cors({
-    origin: [
-      "http://localhost:3000",
-      "https://localhost:3000",
-      "http://km.og.nomoredomainsrocks.ru",
-      "https://km.og.nomoredomainsrocks.ru",
-    ],
-    methods: [
-      "GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH", "HEAD",
-    ],
-  }),
-);
+app.use(cors);
+
+// app.use(
+//   cors({
+//     origin: [
+//       "http://localhost:3000",
+//       "https://localhost:3000",
+//       "http://km.og.nomoredomainsrocks.ru",
+//       "https://km.og.nomoredomainsrocks.ru",
+//     ],
+//     methods: [
+//       "GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH", "HEAD",
+//     ],
+//   }),
+// );
 // подключаем мидлвары, роуты и всё остальное...
 app.use(helmet());
 app.use(express.json());
