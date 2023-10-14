86669eec348d97694251208f7f1489d75c46f854
Inessa Kim <kim.inessa20@yandex.ru>
2023-10-14 23:20:15 +0300

    fix: cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -13,6 +13,8 @@ module.exports = (req, res, next) => {
   const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
   const requestHeaders = req.headers["access-control-request-headers"];
 
+  res.header("Access-Control-Allow-Origin", "*");
+
   if (allowedCors.includes(origin)) {
     res.header("Access-Control-Allow-Origin", origin);
     res.header("Access-Control-Allow-Credentials", "true");
@@ -22,5 +24,5 @@ module.exports = (req, res, next) => {
     res.header("Access-Control-Allow-Headers", requestHeaders);
     return res.end();
   }
-  next();
+  return next();
 };
